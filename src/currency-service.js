513d41c48891124e23b9594b731c897af1b25898
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-23 10:03:15 -0700

    add initial UI logic

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,6 +1,6 @@
 export default class Currency {
   static getCurrency(currency) {
-    return fetch(`https://v6.exchangerate-api.com/v6/API_KEY/latest/USD`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import $ from 'jquery'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/styles.css'
-import PokeService from './currency-service.js'
+import Currency from './currency-service.js'
 
-console.log(PokeService);
+function getExchange(response) {
+  if (response) {
+    
+  }
+}
 
-$ 
+$(document).ready(function() {
+  $('#dollars').submit(function(event) {
+    event.preventDefault();
+    let currency = $('#exchange').val();
+    Currency.getCurrency(currency)
+      .then(function(response) {
+        getExchange(response);
+    })
+  })
+})
